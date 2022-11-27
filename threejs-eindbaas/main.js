a2ecbb56f7c3a3454293a33c107f9ec06d5bec9f
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-11-27 11:57:13 +0100

    feat: load gltf model

--- threejs-eindbaas/main.js
+++ threejs-eindbaas/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 
 
@@ -11,21 +12,37 @@ const scene = new THREE.Scene();
       //set renderer size to half of page size
       renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
 
+      //add ambient light
+      const ambientLight = new THREE.AmbientLight( 0xffffff, 0.5 );
+      scene.add( ambientLight );
 
+      //add directional light
+      const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+      scene.add( directionalLight );
+
+      //add orbit controls
+      const controls = new OrbitControls( camera, renderer.domElement );
+      controls.enableDamping = true;
+
+      //import gltf model from /assets
+      const loader = new GLTFLoader();
+      loader.load( '/assets/gltf/donut.glb', function ( gltf ) {
+        scene.add( gltf.scene );
+      }, undefined, function ( error ) {
+        console.error( error );
+      } );
+
+      
 			document.body.appendChild( renderer.domElement );
 
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
 
+			
 			camera.position.z = 5;
 
 			function animate() {
 				requestAnimationFrame( animate );
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
+	
 
 				renderer.render( scene, camera );
 			};
