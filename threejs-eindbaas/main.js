45879cd64a34e271d663893405fed19deb13167f
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-11-28 21:18:34 +0100

    style: change configurator panel

--- threejs-eindbaas/main.js
+++ threejs-eindbaas/main.js
@@ -10,7 +10,6 @@ const scene = new THREE.Scene();
 			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 			const renderer = new THREE.WebGLRenderer({preserveDrawingBuffer: true});
-      //set renderer size to half of page size
       renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
       document.body.appendChild( renderer.domElement );
 
@@ -32,7 +31,7 @@ const scene = new THREE.Scene();
         console.log(gltf);
         //console log the material type of the glb file
         console.log(gltf.scene.children[0].material);
-        
+
       
       }, undefined, function ( error ) {
         console.error( error );
